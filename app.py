de050f5e23a7945cf7f11639f45cfa5906da5855
M. Khaidar <haidar038@gmail.com>
2025-01-19 00:39:49 +0900

    Changes to be committed: 	modified:   app.py 	modified:   logs/app.log

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from App import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8082, threaded=True)
