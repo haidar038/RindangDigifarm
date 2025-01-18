67f33f5775058e04ca28732fe80b9429c663f919
M. Khaidar <haidar038@gmail.com>
2025-01-19 01:08:33 +0900

    Changes to be committed: 	modified:   passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,15 @@
-from App.run import app as application
+import sys
+import os
+
+VENV = "/home/rind1083/virtualenv/RindangDigifarm/3.11/bin/python"
+if sys.executable != VENV:
+    os.execl(VENV, VENV, *sys.argv)
+
+sys.path.insert(0, '/home/rind1083/RindangDigifarm')
+
+# Import app factory dan Config, bukan ProductionConfig
+from App import create_app
+from App.config import Config
+
+# Gunakan Config biasa
+application = create_app(Config)
